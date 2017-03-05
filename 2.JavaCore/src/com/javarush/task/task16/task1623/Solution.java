ad1908b5f4bdf5749ee571dd290446e5179e50c3
xsd <saleksey67@gmail.com>
2017-03-05 16:21:23 +0300

    tested - ok

---
+++ 2.JavaCore/src/com/javarush/task/task16/task1623/Solution.java
@@ -0,0 +1,33 @@
+package com.javarush.task.task16.task1623;
+
+/* 
+Рекурсивное создание нитей
+*/
+
+import javax.sound.midi.Soundbank;
+
+public class Solution {
+    static int count = 15;
+    static volatile int countCreatedThreads = 1;
+
+    public static void main(String[] args) {
+        System.out.println(new GenerateThread());
+        while (countCreatedThreads <= Solution.count) {
+            System.out.println(new GenerateThread());
+        }
+    }
+
+    public static class GenerateThread extends Thread {
+
+        GenerateThread() {
+            super(String.valueOf(countCreatedThreads));
+            countCreatedThreads++;
+            run();
+        }
+
+        @Override
+        public String toString() {
+            return (getName() + " created");
+        }
+    }
+}
