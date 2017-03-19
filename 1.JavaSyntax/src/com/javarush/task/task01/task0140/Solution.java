d5bb4cba3dce76935d7b3a696d36332febcfb818
xsd <saleksey67@gmail.com>
2017-03-19 13:01:58 +0300

    tested - ok!

--- 1.JavaSyntax/src/com/javarush/task/task01/task0140/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0140/Solution.java
@@ -1,6 +1,5 @@
 package com.javarush.task.task01.task0140;
 
-import java.io.InputStreamReader;
 import java.util.Scanner;
 
 /* 
@@ -11,9 +10,8 @@ public class Solution {
     public static void main(String[] args) {
         int a;
         //напишите тут ваш код
-        Scanner sc = new Scanner(new InputStreamReader(System.in));
+        Scanner sc = new Scanner(System.in);
         a = sc.nextInt();
-
         System.out.println(a * a);
     }
 }
