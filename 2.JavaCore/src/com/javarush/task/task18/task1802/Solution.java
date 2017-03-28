f4deaab09b3827fc28c142994f763d774e91a3b0
xsd <saleksey67@gmail.com>
2017-03-28 12:13:01 +0300

    tested - ok

---
+++ 2.JavaCore/src/com/javarush/task/task18/task1802/Solution.java
@@ -0,0 +1,31 @@
+package com.javarush.task.task18.task1802;
+
+import org.omg.CORBA.portable.InputStream;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
+/* 
+Минимальный байт
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        FileInputStream fileInputStream = new FileInputStream(bufferedReader.readLine());
+
+        int min = fileInputStream.read();
+
+        while (fileInputStream.available() > 0) {
+            int data = fileInputStream.read();
+
+            if (data < min) {
+                min = data;
+            }
+        }
+
+        System.out.println(min);
+        fileInputStream.close();
+    }
+}
