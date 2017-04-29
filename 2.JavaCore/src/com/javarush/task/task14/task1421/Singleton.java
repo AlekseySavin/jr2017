8cfe8ad61ff5e38cbdba68d5dc81cfedeb10279f
xsd <saleksey67@gmail.com>
2017-04-29 11:07:19 +0300

    1421 tested

--- 2.JavaCore/src/com/javarush/task/task14/task1421/Singleton.java
+++ 2.JavaCore/src/com/javarush/task/task14/task1421/Singleton.java
@@ -9,5 +9,16 @@ package com.javarush.task.task14.task1421;
  * @version 0.1
  */
 public class Singleton {
+    private static Singleton instance;
 
+    private Singleton() {
+
+    }
+
+    public static Singleton getInstance() {
+        if (instance == null) {
+            instance = new Singleton();
+        }
+        return instance;
+    }
 }
