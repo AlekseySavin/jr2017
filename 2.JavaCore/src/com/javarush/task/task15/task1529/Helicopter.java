12476edb811ce6d758f6a2f9e34862b5ff2ebbf2
xsd <saleksey67@gmail.com>
2017-04-30 18:54:27 +0300

    task1529 tested!

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1529/Helicopter.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task15.task1529;
+
+/**
+ * Created by xsd on 30.04.2017 with love.
+ * :)
+ *
+ * @author xsd
+ * @author alekseysavin.com
+ * @version 0.1
+ */
+public class Helicopter implements Flyable {
+    @Override
+    public void fly() {
+
+    }
+}
---
+++ 2.JavaCore/src/com/javarush/task/task15/task1529/Plane.java
@@ -0,0 +1,19 @@
+package com.javarush.task.task15.task1529;
+
+/**
+ * Created by xsd on 30.04.2017 with love.
+ * :)
+ *
+ * @author xsd
+ * @author alekseysavin.com
+ * @version 0.1
+ */
+public class Plane implements Flyable {
+    public Plane(int i) {
+    }
+
+    @Override
+    public void fly() {
+
+    }
+}
